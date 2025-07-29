8e655a5592c7b2e891d5c1cab1c9c610276176db
Nitin Pinnamaneni <nitinpinnamaneni@yahoo.com>
2025-07-29 14:16:03 -0400

    Displayed images for apartments and left and right arrows

--- ApartmentDetails.js
+++ ApartmentDetails.js
@@ -7,16 +7,37 @@ import { FlatList } from 'react-native-web';
 
 const CustomCarousel = ({ images }) => {
  
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const goNext = () => {
+    if (currentIndex < images.length - 1) {
+        setCurrentIndex(currentIndex + 1);
+    }
+  };
 
+  const goPrev = () => {
+    if (currentIndex > 0) {
+        setCurrentIndex(currentIndex - 1);
+    }
+  };
   return (
     <View style={styles.carouselWrapper}>
       {/* Left Arrow */}
-    
+      <TouchableOpacity onPress={goPrev} style={styles.arrowLeft}>
+        <Ionicons name="arrow-back" size={24} color="white" />
+      </TouchableOpacity>
 
       {/* Image Carousel */}
-     
+      <ScrollView
+             horizontal
+             style={styles.carouselContainer}
+      >
+         <Image key={currentIndex} source={{ uri: images[currentIndex] }} style={styles.carouselImage} />
+      </ScrollView>
 
       {/* Right Arrow */}
+      <TouchableOpacity onPress={goNext} style={styles.arrowRight}>
+        <Ionicons name="arrow-forward" size={24} color="white" />
+      </TouchableOpacity>
       
     </View>
   );
